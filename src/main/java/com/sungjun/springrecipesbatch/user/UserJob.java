ddac13417cfd341001b8fe3b5a551bbe9bc1c5ae
Sungjun <sungjunpizz@gmail.com>
2019-02-12 00:05:55 +0900

    ItemReader/ItemWriter 간단하게 구성하기

--- src/main/java/com/sungjun/springrecipesbatch/user/UserJob.java
+++ src/main/java/com/sungjun/springrecipesbatch/user/UserJob.java
@@ -7,7 +7,9 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.util.ClassUtils;
 
 import javax.sql.DataSource;
 
@@ -44,7 +47,7 @@ public class UserJob {
     @Bean
     public Step step1() {
         return steps.get("User Registration CSV To DB Step")
-                .<UserRegistration, UserRegistration> chunk(5)
+                .<UserRegistration, UserRegistration>chunk(5)
                 .reader(csvFileReader())
                 .writer(jdbcItemWriter())
                 .build();
@@ -52,43 +55,22 @@ public class UserJob {
 
     @Bean
     public FlatFileItemReader<UserRegistration> csvFileReader() {
-        FlatFileItemReader<UserRegistration> itemReader = new FlatFileItemReader<>();
-        itemReader.setLineMapper(lineMapper());
-        itemReader.setResource(input);
-        return itemReader;
+        return new FlatFileItemReaderBuilder<UserRegistration>()
+                .name(ClassUtils.getShortName(FlatFileItemReader.class))
+                .resource(input)
+                .targetType(UserRegistration.class)
+                .delimited()
+                .names(new String[]{"firstName", "lastName", "company", "address", "city", "state", "zip", "county", "url", "phoneNumber", "fax"})
+                .build();
     }
 
     @Bean
     public JdbcBatchItemWriter<UserRegistration> jdbcItemWriter() {
-        JdbcBatchItemWriter<UserRegistration> itemWriter = new JdbcBatchItemWriter<>();
-        itemWriter.setDataSource(dataSource);
-        itemWriter.setSql(INSERT_REGISTRATION_QUERY);
-        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
-        return itemWriter;
+        return new JdbcBatchItemWriterBuilder<UserRegistration>()
+                .dataSource(dataSource)
+                .sql(INSERT_REGISTRATION_QUERY)
+                .beanMapped()
+                .build();
     }
-
-    @Bean
-    public DefaultLineMapper<UserRegistration> lineMapper() {
-        DefaultLineMapper<UserRegistration> lineMapper = new DefaultLineMapper<>();
-        lineMapper.setLineTokenizer(tokenizer());
-        lineMapper.setFieldSetMapper(fieldSetMapper());
-        return lineMapper;
-    }
-
-    @Bean
-    public DelimitedLineTokenizer tokenizer() {
-        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
-        tokenizer.setDelimiter(",");
-        tokenizer.setNames("firstName","lastName","company","address","city","state","zip","county","url","phoneNumber","fax");
-        return tokenizer;
-    }
-
-    @Bean
-    public BeanWrapperFieldSetMapper<UserRegistration> fieldSetMapper() {
-        BeanWrapperFieldSetMapper<UserRegistration> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
-        fieldSetMapper.setTargetType(UserRegistration.class);
-        return fieldSetMapper;
-    }
-
 
 }
