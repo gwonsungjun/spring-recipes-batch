e39ae0e47f5d5dc430cc0ecaeedf3c75ac8a63f9
Sungjun <sungjunpizz@gmail.com>
2019-02-11 21:48:24 +0900

    Init repo

---
+++ src/main/java/com/sungjun/springrecipesbatch/SpringrecipesBatchApplication.java
@@ -0,0 +1,14 @@
+package com.sungjun.springrecipesbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipesBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrecipesBatchApplication.class, args);
+    }
+
+}
+
